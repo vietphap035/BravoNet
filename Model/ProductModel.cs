72c76b10b71866279057de56240308e60b6931cf
Viet_Phap <khskop7@gmail.com>
2025-07-11 23:52:02 +0700

    Add new model

---
+++ Model/ProductModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DACS_1.Model
+{
+    public class ProductModel
+    {
+        public int P_Id { get; set; }
+        public string P_Name { get; set; }
+        public decimal P_Price { get; set; }
+        public int P_Quantity { get; set; }
+    }
+}
---
+++ Model/StaffModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DACS_1.Model
+{
+    public class StaffModel
+    {
+        public string UId { get; set; }
+        public string FullName { get; set; }
+        public decimal basicSalary { get; set; }
+        public decimal bonus { get; set; }
+
+        public int workingHours { get; set; }
+
+        public decimal salary
+        {
+            get
+            {
+                return basicSalary + bonus + (workingHours * 10000); // Assuming 10,000 VND per hour
+            }
+        }
+    }
+}
