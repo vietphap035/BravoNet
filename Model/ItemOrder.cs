bedabe66c74b87b7b819bff9eb8d46d18ac15426
Viet.Phap <khskop7@gmail.com>
2025-07-25 12:05:41 +0700

    add new model

---
+++ Model/ItemOrder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DACS_1.Model
+{
+    public class ItemOrder
+    {
+        public int Id { get; set; }
+        public string name { get; set; }
+        public int quantity { get; set; }
+        public decimal price_at_order { get; set; }
+    }
+}
--- Model/OrderModel.cs
+++ Model/OrderModel.cs
@@ -14,6 +14,7 @@ namespace DACS_1.Model
             // Nên dùng DateTime để lưu ngày giờ
             public DateTime OrderDate { get; set; }
 
+            public string UserName { get; set; }
             public bool Status { get; set; }
         }
 
